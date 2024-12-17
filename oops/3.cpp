397ba5f5e05bac7f4eb78c58e62b2b3a7ee005ab
Venk8esh <134226907+venkateshpagdikar@users.noreply.github.com>
2024-12-17 14:13:08 +0530

    Update 3.cpp

--- oops/3.cpp
+++ oops/3.cpp
@@ -1,16 +1,13 @@
-#include <iostream>
-using namespace std;
+d#include <iostream.h>
 
 class acc {
     int no;
-    float bal; // Changed to float to handle decimal values
+    float bal;
 
 public:
-    // Constructor for initializing account
     acc() : no(0), bal(0) {}
     acc(int ain, float bin = 0) : no(ain), bal(bin) {}
 
-    // Method to manually get account details
     void get() {
         cout << "\nEnter the account number: ";
         cin >> no;
@@ -18,13 +15,11 @@ public:
         cin >> bal;
     }
 
-    // Display account information
     void display() const {
         cout << "\nThe Account number: " << no;
         cout << "\nBalance: " << bal;
     }
 
-    // Transfer money between accounts
     void transfer(acc &a, float amt) {
         if (amt > bal) {
             cout << "\nTransfer failed: Insufficient balance in account " << no << ".\n";
@@ -39,23 +34,19 @@ public:
 
 int main() {
     float amount;
-    acc a1, a2(2), a3(3, 100.5); // Using constructors for initialization
+    acc a1, a2(2), a3(3, 100.5);
 
-    // Input details for the first account
     a1.get();
 
-    // Display initial account information
     cout << "\nAccount information \n";
     a1.display();
     a2.display();
     a3.display();
 
-    // Transfer money from account 1 to account 2
     cout << "\nHow much money is to be transferred from account 1 to account 2: ";
     cin >> amount;
     a1.transfer(a2, amount);
 
-    // Display updated account information
     cout << "\nUpdated account information \n";
     a1.display();
     a2.display();
